c0312b95b9d392b38ec5b7ffd0f9afb6bd17a02b
vinandamarbun01 <vinandakristin290@gmail.com>
2025-09-17 09:47:52 +0700

    250441100112_Vinandakristianimarbun_Day1

---
+++ 250441100112_Vinanda_Day1/py.day1.py
@@ -0,0 +1,20 @@
+# Program Menghitung Nilai Akhir
+
+# Nilai yang diperoleh Andi
+nilai_tugas = 85
+nilai_uts = 75
+nilai_uas = 90
+
+# Bobot nilai
+bobot_tugas = 0.30
+bobot_uts = 0.30
+bobot_uas = 0.40
+
+# Hitung nilai akhir
+nilai_akhir = (nilai_tugas * bobot_tugas) + (nilai_uts * bobot_uts) + (nilai_uas * bobot_uas)
+
+# Tampilkan hasil
+print("Nilai Tugas :", nilai_tugas)
+print("Nilai UTS :", nilai_uts)
+print("Nilai UAS :", nilai_uas)
+print("Nilai Akhir :", nilai_akhir)
